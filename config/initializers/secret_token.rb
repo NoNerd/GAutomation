cfe4a77d106bc08b92fc71a21000510273c71740
unknown <lvyanliang@.ds.gome.com.cn>
2013-06-03 18:44:56 +0800

    6.3 work

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+GAutomation::Application.config.secret_token = '5a759971ba5c73c7e23fea2833402c546fa6fdcd2f989f529066fd7ba5817df29d7838b1b78981f288b46eb1812d17adbca234a149b466544c1b33b58b9e73f3'
